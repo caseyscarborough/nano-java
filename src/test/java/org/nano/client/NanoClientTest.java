75b7edc24a5ceb08834d8cce6f81a22fd258a5cb
Casey Scarborough <caseyscarborough@gmail.com>
2018-02-03 13:09:17 -0500

    Add more tests

--- src/test/java/org/nano/client/NanoClientTest.java
+++ src/test/java/org/nano/client/NanoClientTest.java
@@ -17,8 +17,10 @@ import static org.mockito.Mockito.when;
 public class NanoClientTest {
 
     private static final String ACCOUNT = "xrb_3t6k35gi95xu6tergt6p69ck76ogmitsa8mnijtpxm9fkcm736xtoncuohr3";
-    private static final String INVALID_ACCOUNT = "invalid_account";
+    private static final String PUBLIC_KEY = "E89208DD038FBB269987689621D52292AE9C35941A7484756ECCED92A65093BA";
+    private static final String WALLET = "01028E84A43D7840A4DE32DF7EE0189819062269FE76A43A4FC560ACA35264AC";
     private static final String BAD_ACCOUNT_NUMBER_ERROR = "Bad account number";
+    private static final String WALLET_NOT_FOUND_ERROR = "Wallet not found";
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -30,6 +32,7 @@ public class NanoClientTest {
     public void setUp() throws Exception {
         httpClient = mock(HttpClient.class);
         client = new NanoClient(httpClient);
+//        client = new NanoClient();
     }
 
     @Test
@@ -48,7 +51,7 @@ public class NanoClientTest {
 
         expectedException.expect(NanoException.class);
         expectedException.expectMessage(BAD_ACCOUNT_NUMBER_ERROR);
-        client.getAccountBalance(INVALID_ACCOUNT);
+        client.getAccountBalance(ACCOUNT);
     }
 
     @Test
@@ -64,7 +67,7 @@ public class NanoClientTest {
 
         expectedException.expect(NanoException.class);
         expectedException.expectMessage(BAD_ACCOUNT_NUMBER_ERROR);
-        client.getAccountBlockCount(INVALID_ACCOUNT);
+        client.getAccountBlockCount(ACCOUNT);
     }
 
     @Test
@@ -85,7 +88,63 @@ public class NanoClientTest {
 
         expectedException.expect(NanoException.class);
         expectedException.expectMessage(BAD_ACCOUNT_NUMBER_ERROR);
-        client.getAccountInformation(INVALID_ACCOUNT);
+        client.getAccountInformation(ACCOUNT);
+    }
+
+    @Test
+    public void testCreateAccount() throws Exception {
+        expectJson("account_create");
+
+        AccountCreate account = client.createAccount(WALLET);
+        assertEquals("xrb_1rophzebpsxhitm6cphr8qtph4gzjez899ibetqn1bftqydpp9yzau7fkhtm", account.getAccount());
+    }
+
+    @Test
+    public void testCreateAccountWhenWalletNotFound() throws Exception {
+        expectJson("account_create_wallet_not_found");
+
+        expectedException.expect(NanoException.class);
+        expectedException.expectMessage(WALLET_NOT_FOUND_ERROR);
+        client.createAccount(WALLET);
+    }
+
+    @Test
+    public void testGetAccountPublicKey() throws Exception {
+        expectJson("account_key");
+
+        AccountPublicKey pk = client.getAccountPublicKey(ACCOUNT);
+        assertEquals("E89208DD038FBB269987689621D52292AE9C35941A7484756ECCED92A65093BA", pk.getKey());
+    }
+
+    @Test
+    public void testGetAccount() throws Exception {
+        expectJson("account_get");
+
+        AccountGet account = client.getAccount(PUBLIC_KEY);
+        assertEquals(ACCOUNT, account.getAccount());
+    }
+
+    @Test
+    public void testGetAccountHistory() throws Exception {
+        expectJson("account_history");
+
+        AccountHistory history = client.getAccountHistory(ACCOUNT, 10);
+        assertEquals(10, history.getHistory().size());
+
+        History h = history.getHistory().get(0);
+        assertEquals("xrb_1111111111111111111111111111111111111111111111111111hifc8npp", h.getAccount());
+        assertEquals("205676479000000000000000000000000000000", h.getAmount());
+        assertEquals("ECCB8CB65CD3106EDA8CE9AA893FEAD497A91BCA903890CBD7A5C59F06AB9113", h.getHash());
+        assertEquals("send", h.getType());
+    }
+
+    @Test
+    public void testGetAccountList() throws Exception {
+        expectJson("account_list");
+
+        AccountList list = client.getAccountList(WALLET);
+        assertEquals(3, list.getAccounts().size());
+        assertEquals(ACCOUNT, list.getAccounts().get(0));
     }
 
     private void expectJson(String name) throws IOException {
